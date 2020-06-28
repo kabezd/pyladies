8da68f3aef5d1e9b9f0f4e50cf57423605196597
Katka Bezdekova <bezdekova.k@seznam.cz>
2020-06-28 18:57:32 +0200

    ukolu z lekce 6

---
+++ ukoly/lekce_06_funkce/8_hod_kostkou.py
@@ -0,0 +1,28 @@
+from random import randrange
+
+
+pocet_hracu = 4
+vysledky = {} # prazdny slovnik, key = cislo hrace, value = pocet hodu
+
+
+print("Zacina hra s", pocet_hracu, "hraci")
+
+for i in range(1, pocet_hracu +1):
+    print()
+    print("Je na rade hrac cislo: ", i)
+    pocet_hodu = 0
+    while(True):
+        hod = randrange(1,7)
+        pocet_hodu += 1
+        print("Hod cislo: ", pocet_hodu, "Padla hodnota: ", hod)
+        if(hod ==6):
+            vysledky[i] = pocet_hodu
+            break
+
+# seradit dle poctu hodu
+serazene = sorted(vysledky.items(), key=lambda x: x[1], reverse=True)
+
+# kontrola
+print("serazene:", serazene)
+print(" Vyhercem je hrac cislo: ", next(iter(serazene)))
+print(" Vyhercem je hrac cislo: ", list(serazene)[0][0])
