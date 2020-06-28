bea25deaea4c2a9e1dd5482eb9b5809d954d93a6
Katka Bezdekova <bezdekova.k@seznam.cz>
2020-06-28 18:59:51 +0200

    uprava testu piskvorky

--- ukoly/lekce_07_testovani/piskvorky_refactored/test_piskvorky.py
+++ ukoly/lekce_07_testovani/piskvorky_refactored/test_piskvorky.py
@@ -1,1 +1,13 @@
-# zde jsou testy funkci piskvorek
+import piskvorky
+
+def test_zadani_na_spravnou_pozici():
+    pole = piskvorky.tah('--------------------', 5, "x" )
+    assert len(pole) == 20
+    assert pole[5] == "x"
+    assert pole.count('-') == 19
+
+def test_tah_na_prazdne_pole():
+    pole = piskvorky.tah_pocitace('--------------------')
+    assert len(pole) == 20
+    assert pole.count('o') == 1
+    assert pole.count('-') == 19
